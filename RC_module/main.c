9d2d16eeac0ac9b8cfc8ce84766911d0f21241d0
reaper20051 <2023.shlok.shetty@ves.ac.in>
2026-04-10 17:18:36 +0530

    Update main.c

--- RC_module/main.c
+++ RC_module/main.c
@@ -1,27 +1,20 @@
 #include "ibus.h"
 #include <stdio.h>
-
 // If your SDK doesn't provide a global tick, we use a loop counter for timing
 static unsigned int fake_tick = 0;
-
 void main(void) {
     // Array to store the 10 channels from the FS-iA10B
     unsigned short rc_values[IBUS_NUM_CHANNELS];
     unsigned int last_print_tick = 0;
-
     // Initialize UART1 for 115200 baud
     ibus_init();
-
     printf("\r\n=== Trisul-SoC iBUS RC Test ===\r\n");
-
     while (1) {
         // 1. Pass the array to the function (Fixes the "too few arguments" error)
         // This returns 1 only when a complete, valid frame is received
         int valid_frame = ibus_read(rc_values);
-
         // Increment a counter to simulate timing for printing
         fake_tick++;
-
         // 2. Non-blocking Print (every few thousand loops)
         if (fake_tick - last_print_tick > 5000) {
             if (valid_frame || rc_values[0] != 0) {
@@ -41,3 +34,6 @@ void main(void) {
         }
     }
 }
+
+
+this is my main.c
